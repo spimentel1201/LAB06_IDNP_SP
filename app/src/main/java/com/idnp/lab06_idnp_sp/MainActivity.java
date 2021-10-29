0d71c91918fa3e62b695f80c9198c7d600d265d9
Sonny <spimentelm1201@gmail.com>
2021-10-29 14:13:19 -0500

    Creación del Proyecto

---
+++ app/src/main/java/com/idnp/lab06_idnp_sp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.idnp.lab06_idnp_sp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
